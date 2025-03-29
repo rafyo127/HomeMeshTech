e438d622069defff1ddee95a765de245fe525a14
rafyo127 <rafel_2009@live.com>
2025-03-29 01:35:44 -0400

    new addon for mDNS

---
+++ mdns-repeater/run.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+SOURCE_INTERFACE=$(jq -r '.source_interface' /data/options.json)
+DESTINATION_INTERFACE=$(jq -r '.destination_interface' /data/options.json)
+
+echo "[INFO] Starting mDNS repeater..."
+echo "[INFO] Source Interface: $SOURCE_INTERFACE"
+echo "[INFO] Destination Interface: $DESTINATION_INTERFACE"
+
+exec mdns-repeater "$SOURCE_INTERFACE" "$DESTINATION_INTERFACE"
