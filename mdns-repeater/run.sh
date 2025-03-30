226d1f09cb404a5ea45f8ce04b1c8967ddacbc18
rafyo127 <rafel_2009@live.com>
2025-03-29 21:35:06 -0400

    Update run.sh

--- mdns-repeater/run.sh
+++ mdns-repeater/run.sh
@@ -1,11 +1,9 @@
 #!/bin/sh
 
-echo "[DEBUG] Installed packages:"
+echo "[INFO] Installed packages:"
 apk info
 
-echo "[DEBUG] Checking for jq:"
-which jq
-
+echo "[INFO] Reading configuration..."
 SOURCE_INTERFACE=$(jq -r '.source_interface' /data/options.json)
 DESTINATION_INTERFACE=$(jq -r '.destination_interface' /data/options.json)
 
@@ -13,8 +11,4 @@ echo "[INFO] Starting mDNS repeater..."
 echo "[INFO] Source Interface: $SOURCE_INTERFACE"
 echo "[INFO] Destination Interface: $DESTINATION_INTERFACE"
 
-# Start the mDNS repeater
-mdns-repeater "$SOURCE_INTERFACE" "$DESTINATION_INTERFACE" &
-
-# Wait to keep container alive (in case the binary exits too quickly)
-wait $!
+exec mdns-repeater "$SOURCE_INTERFACE" "$DESTINATION_INTERFACE"
